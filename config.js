3ca0eb45123768417c5acf39e5828f1807e90e14
aomii <aomsin140447@gmail.com>
2026-04-16 12:17:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "เย่ๆๆๆ 🎉💝💖💝💓",
-        message: "มาถึงตรงนี้แปลว่ารักกันแล้วน้า เอ้ย ดีกานแน้ว Miss you 3000",
+        message: "มาถึงตรงนี้แปลว่ารักกันแล้วน้า เอ้ย ดีกานแน้ว ิ ิ Miss you 3000",
         emojis: "🎁💖🤗💝❤️💕"  // These will bounce around
     },
 
